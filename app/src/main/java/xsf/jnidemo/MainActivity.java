1d691e1e13a70027858ffb7969e95e7745680e26
hzxushangfei <hzxushangfei@corp.netease.com>
2017-04-13 19:36:46 +0800

    测试新的feature

--- app/src/main/java/xsf/jnidemo/MainActivity.java
+++ app/src/main/java/xsf/jnidemo/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    //测试feature分支
     private Button btnJni1, btnJni2, btnJni3;
     private TextView tvShowInfo;
     private NDKUtils jniUtil;
